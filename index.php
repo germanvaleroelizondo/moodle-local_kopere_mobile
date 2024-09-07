b62275e790e13e16a4c5dccae4fe215e1dde77de
Eduardo Kraus <kraus@eduardokraus.com>
2024-09-07 16:43:00 -0300

    Code checker

--- index.php
+++ index.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// No login check is expected here bacause token validation.
+// @codingStandardsIgnoreLine
 require ("../../config.php");
 
 header("Cache-Control: no-cache, must-revalidate");
